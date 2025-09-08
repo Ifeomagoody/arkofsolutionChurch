d8ac45a82220a8da99e67494342b76527bf6d553
Ifeomagoody <goodnessezeokoli3@gmail.com>
2025-09-08 15:23:04 +0100

    the HearOurSermonPage was added

---
+++ src/components/HeroSection/HearOurSermonsPage.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+export default function HearOurSermonsPage() {
+  return (
+    <>
+      <section className=' bg-[#1E3A8A] h-[40vh] flex   justify-around items-center '>
+        <div className='flex flex-wrap gap-15'> 
+         <h1 className='font-bold text-[40px] text-[#fff] '>GET BETTER BY HEARING OUR SERMONS</h1>
+         <button className=' text-xs font-bold text-[#e7dada] rounded-full py-2 px-10 cursor-pointer bg-[#3b82f6f6] hover:bg-white hover:text-[#3b82f6f6]'>JOIN US</button>
+        </div>
+      </section>
+    </>
+  )
+}
