8c7abb3b370d6c8026e1716a0326f8d0993a0415
Ifeomagoody <goodnessezeokoli3@gmail.com>
2025-09-08 18:31:17 +0100

    the Footer page was added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,70 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div className='border border-red-500 text-black'>
-        I am the Footer
+    <section className=''> 
+
+    <div className='flex  bg-[#fff] gap-10 flex-wrap h-[60vh] pt-20 items-center justify-around'>
+        <div className=''>
+          <h3 className='text-[#1E3A8A] font-medium text-base'>CONTACT</h3>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>43 Raymouth Rd. Baltemoer, London 3910</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>+(234) 803 573 0419</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>+(234) 803 573 0419</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>info@arkofsolution.com</li>
+        </div>
+      
+        <div className=''>
+          <h3 className='text-[#1E3A8A] font-medium text-base'>SOURCES</h3>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>About us</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Services</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Vision</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Mission</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Terms</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Privacy</li>
+          </div>
+          <div className=''>
+    
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Partners</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Business</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Careers</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Blog</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>FAQ</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Creative</li>
+        </div>
+       
+
+        <div className='pl-5'>
+          <h3 className='text-[#1E3A8A] font-medium text-base'>LINKS</h3>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Our Vision</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>About us</li>
+          <li className='underline text-[14px] font-medium decoration-[#888] text-[#888] underline-offset-4 list-none hover:underline transition delay-75 hover:decoration-[#1E3A8A]'>Contact us</li>
+          
+          <div className='flex py-5 gap-1'>
+                <div className='w-10 h-10 rounded-full relative bg-[#1E3A8A] text-white hover:bg-[#3b82f6f6]'>
+                  <img src='public/instagram-brands-solid-full.svg' className='absolute top-2.5 w-10 h-5'/>
+                </div>
+                <div className='w-10 h-10 rounded-full relative bg-[#1E3A8A] text-white hover:bg-[#3b82f6f6]'>
+                  <img src='public/twitter-brands-solid-full.svg' className='absolute top-2.5 w-10 h-6'/>
+                </div>
+                <div className='w-10 h-10 rounded-full relative bg-[#1E3A8A] text-white hover:bg-[#3b82f6f6]'>
+                  <img src='public/facebook-f-brands-solid-full.svg' className='absolute top-2.5 w-10 h-6'/>
+                </div>
+                <div className='w-10 h-10 rounded-full relative bg-[#1E3A8A] text-white hover:bg-[#3b82f6f6]'>
+                  <img src='public/linkedin-in-brands-solid-full.svg' className='absolute top-2.5 w-10 h-6'/>
+                </div>
+                <div className='w-10 h-10 rounded-full relative bg-[#1E3A8A] text-white hover:bg-[#3b82f6f6]'>
+                  <img src='public/pinterest-brands-solid-full.svg' className='absolute top-2 w-10 h-6'/>
+                </div>
+                <div className='w-10 h-10 rounded-full relative bg-[#1E3A8A] text-white hover:bg-[#3b82f6f6]'>
+                  <img src='public\dribbble-brands-solid-full.svg' className='absolute top-2 w-10 h-6'/>
+                </div>
+          </div>
+
+        </div>
     </div>
+              <div className='flex flex-col pb-15 justify-center items-center'>
+
+          <h2>Copyright ©2025. All Rights Reserved. — Designed with love by <span className=' hover:text-[#1e3a8a] transition delay-150'>Ifeoma Ezeokoli👑</span></h2>
+              </div>
+    </section>
   )
 }
